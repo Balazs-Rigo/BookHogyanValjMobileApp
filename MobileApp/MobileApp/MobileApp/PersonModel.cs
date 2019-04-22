5280fb76d2d00bf1d4f2d94a6116bff7fb2f5add
Balázs Rigó <rigo.balazs86@gmail.com>
2019-04-22 20:51:16 +0200

    Az elobb kimaradt, hogy a 149.es kodot is implementaltam es most a 150es is felkerult.

---
+++ MobileApp/MobileApp/MobileApp/PersonModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MobileApp
+{
+    class PersonModel : ObservableObject
+    {
+        private string _id,
+
+        public string ID
+        {
+
+            get { return _id}
+            set
+            {
+                _id = value;
+                NotifyPropertyChanged(nameof(ID));
+            }
+        }
+
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                NotifyPropertyChanged(nameof(Name));
+            }
+        }
+
+        private int _age;
+
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                _age = value;
+                NotifyPropertyChanged(nameof(Age));
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0},{1},{2}", _id,_name,_age);
+        }
+
+    }
+}
