1e5f7382469e180ba0b7d394e2e4596cb644265f
Balázs Rigó <rigo.balazs86@gmail.com>
2019-04-24 00:39:47 +0200

    Mappak es fajlok letrehozva a PCL-ben. 160. oldal. 25. abra

---
+++ MobileApp/MobileApp/MobileApp.Android/Views/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MobileApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        private FormModel Model = new FormModel();
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            this.BindingContext = Model;
+        }
+             
+
+        private void PeopleList_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            DisplayAlert("The selected person",peopleList.SelectedItem.ToString(),"Cancel");
+        }
+    }
+}
